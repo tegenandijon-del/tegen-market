a2a293d18c86b0ee8a960835a29a4a91cea77d3e
tegenandijon-del <tegenandijon@gmail.com>
2026-09-15 13:48:35 +0500

    Create module.ts

---
+++ server/src/module.ts
@@ -0,0 +1,8 @@
+// server/src/category.controller.ts
+import { CategoryService } from './category.service';
+
+// server/src/user.controller.ts
+import { UserService } from './user.service';
+
+// server/src/main.ts
+import { AppModule } from './app.module';
