40b5563f5baf6bc9b96fd57dc605a31364735842
tegenandijon-del <tegenandijon@gmail.com>
2026-09-16 00:14:04 +0500

    Create user.service.ts

---
+++ server/src/user.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+}
